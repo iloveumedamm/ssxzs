45e00300d00e447ee0889fecaeffc8a190c84624
streaam-developer <sonukumarkrbbu61@gmail.com>
2024-05-07 18:32:02 +0530

    0

---
+++ FileStream/bot/plugins/stream.py
@@ -0,0 +1,99 @@
+
+import asyncio
+from FileStream.bot import FileStream, multi_clients
+from FileStream.utils.bot_utils import is_user_banned, is_user_exist, is_user_joined, gen_link, gen_linkx, is_channel_banned, is_channel_exist, is_user_authorized
+from FileStream.utils.database import Database
+from FileStream.utils.file_properties import get_file_ids, get_file_info
+from FileStream.config import Telegram
+from pyrogram import filters, Client
+from pyrogram.errors import FloodWait
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.enums.parse_mode import ParseMode
+db = Database(Telegram.DATABASE_URL, Telegram.SESSION_NAME)
+
+@FileStream.on_message(
+    filters.private
+    & (
+            filters.document
+            | filters.video
+            | filters.video_note
+            | filters.audio
+            | filters.voice
+            | filters.animation
+            | filters.photo
+    ),
+    group=4,
+)
+async def private_receive_handler(bot: Client, message: Message):
+    if not await is_user_authorized(message):
+        return
+    if await is_user_banned(message):
+        return
+
+    await is_user_exist(bot, message)
+    if Telegram.FORCE_SUB:
+        if not await is_user_joined(bot, message):
+            return
+    try:
+        inserted_id = await db.add_file(get_file_info(message))
+        await get_file_ids(False, inserted_id, multi_clients, message)
+        reply_markup, stream_text = await gen_link(_id=inserted_id)
+        await message.reply_text(
+            text=stream_text,
+            parse_mode=ParseMode.HTML,
+            disable_web_page_preview=True,
+            reply_markup=reply_markup,
+            quote=True
+        )
+    except FloodWait as e:
+        print(f"Sleeping for {str(e.value)}s")
+        await asyncio.sleep(e.value)
+        await bot.send_message(chat_id=Telegram.ULOG_CHANNEL,
+                               text=f"Gᴏᴛ FʟᴏᴏᴅWᴀɪᴛ ᴏғ {str(e.value)}s ғʀᴏᴍ [{message.from_user.first_name}](tg://user?id={message.from_user.id})\n\n**ᴜsᴇʀ ɪᴅ :** `{str(message.from_user.id)}`",
+                               disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
+
+
+@FileStream.on_message(
+    filters.channel
+    & ~filters.forwarded
+    & ~filters.media_group
+    & (
+            filters.document
+            | filters.video
+            | filters.video_note
+            | filters.audio
+            | filters.voice
+            | filters.photo
+    )
+)
+async def channel_receive_handler(bot: Client, message: Message):
+    if await is_channel_banned(bot, message):
+        return
+    await is_channel_exist(bot, message)
+
+    contr = f"Adding To Db....."
+    await message.reply_text(contr)
+
+    try:
+        inserted_id = await db.add_file(get_file_info(message))
+        await get_file_ids(False, inserted_id, multi_clients, message)
+        reply_markup, stream_text = await gen_linkx(_id=inserted_id)
+        await bot.send_photo(
+            photo=Telegram.MOVIE_PIC,
+            chat_id=Telegram.INDEX_CHANNEL,
+            caption=stream_text,
+            parse_mode=ParseMode.HTML
+        )
+        
+
+    except FloodWait as w:
+        print(f"Sleeping for {str(w.x)}s")
+        await asyncio.sleep(w.x)
+        await bot.send_message(chat_id=Telegram.ULOG_CHANNEL,
+                               text=f"ɢᴏᴛ ғʟᴏᴏᴅᴡᴀɪᴛ ᴏғ {str(w.x)}s ғʀᴏᴍ {message.chat.title}\n\n**ᴄʜᴀɴɴᴇʟ ɪᴅ :** `{str(message.chat.id)}`",
+                               disable_web_page_preview=True)
+    except Exception as e:
+        await bot.send_message(chat_id=Telegram.ULOG_CHANNEL, text=f"**#EʀʀᴏʀTʀᴀᴄᴋᴇʙᴀᴄᴋ:** `{e}`",
+                               disable_web_page_preview=True)
+        print(f"Cᴀɴ'ᴛ Eᴅɪᴛ Bʀᴏᴀᴅᴄᴀsᴛ Mᴇssᴀɢᴇ!\nEʀʀᴏʀ:  **Gɪᴠᴇ ᴍᴇ ᴇᴅɪᴛ ᴘᴇʀᴍɪssɪᴏɴ ɪɴ ᴜᴘᴅᴀᴛᴇs ᴀɴᴅ ʙɪɴ Cʜᴀɴɴᴇʟ!{e}**")
+
---
+++ FileStream/config.py
@@ -0,0 +1,52 @@
+from os import environ as env
+import re
+id_pattern = re.compile(r'^.\d+$')
+import os
+
+class Telegram:
+    API_ID = int(env.get("API_ID", "24010108"))
+    API_HASH = str(env.get("API_HASH", "8d89700b2fc09a3aa6c906cbed65b040"))
+    BOT_TOKEN = str(env.get("BOT_TOKEN", "6609449380:AAGle29QwW3faG8wxXCGFDGVFEVoEfc-hyw"))
+    BASE_SITE = env.get('BASE_SITE', 'falconfiles.in')
+    METHOD = env.get("METHOD", "shortener")
+    API_KEY = env.get('API_KEY', "6d03cea4840640ac95c24ad6f233b4055b812f09")
+    OWNER_ID = int(env.get('OWNER_ID', '5791145987'))
+    WORKERS = int(env.get("WORKERS", "30"))  # 6 workers = 6 commands at once
+    DATABASE_URL = str(env.get('DATABASE_URL', "mongodb+srv://sonu:19040456@cluster0.hwk6ivg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
+    BOT_USERNAME = env.get("BOT_USERNAME", "Falconfilesbot")
+    UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "Falconfiles"))
+    SESSION_NAME = str(env.get('SESSION_NAME', '5481'))
+    FORCE_SUB_ID = env.get('FORCE_SUB_ID', "-1002083734102")
+    FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', 0)
+    FORCE_SUB = True if str(FORCE_SUB).lower() == "true" else False
+    SLEEP_THRESHOLD = int(env.get("SLEEP_THRESHOLD", "30"))
+    FILE_PIC = env.get('FILE_PIC')
+    START_PIC = env.get('START_PIC')
+    VERIFY_PIC = env.get('VERIFY_PIC')
+    DELETE_TIME = int(env.get('AUTO_DELETE_TIME', 1800))
+    MOVIE_PIC = (env.get('MOVIE_PIC', "https://graph.org/file/70b6bbd733b2b2dffd96f.png")).split()
+    VERIFIED_PIC = env.get('VERIFIED_PIC')
+    MULTI_CLIENT = True
+    GROUP = int(env.get("GROUP", "-1004142352920"))
+    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", "-1002090524947"))   # Logs channel for file logs
+    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", "-1002000855166"))   # Logs channel for user logs
+    MODE = env.get("MODE", "primary")
+    SECONDARY = True if MODE.lower() == "secondary" else False
+    AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "")).split()))
+    ADMINS_PERSONS = list(set(int(x) for x in str(env.get("ADMINS_PERSONS", "5791145987")).split()))
+    UPLOAD_CHANNEL = list(set(int(x) for x in str(env.get("UPLOAD_CHANNEL", "-1002145329802 -1002050269288")).split()))
+    INDEX_CHANNEL = int(env.get("UPDATE_CHANNEL", "-1002093160666"))   #-1002083903461
+    
+class Server:
+    PORT = int(env.get("PORT", 6778))
+    BIND_ADDRESS = str(env.get("BIND_ADDRESS", "0.0.0.0"))
+    PING_INTERVAL = int(env.get("PING_INTERVAL", "1200"))
+    HAS_SSL = str(env.get("HAS_SSL", "1").lower()) in ("1", "true", "t", "yes", "y")
+    NO_PORT = str(env.get("NO_PORT", "1").lower()) in ("1", "true", "t", "yes", "y")
+    FQDN = str(env.get("FQDN", "streaamstore.shop"))
+    URL = "http{}://{}{}/".format(
+        "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
+    )
+
+
+MULTI_TOKEN1 = str(env.get("MULTI_TOKEN1", "6342113383:AAHjIB0upZ0RTPvhRbiI14wKvska4rmI-Zg"))
